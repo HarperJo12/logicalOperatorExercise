f8c94a542f00470d833a68f91a4fc0e19563ca84
HarperJo12 <68649970+HarperJo12@users.noreply.github.com>
2021-01-22 08:01:32 +0700

    Add files via upload
    just basic python exercise

---
+++ exerciseLogicalOperator.py
@@ -0,0 +1,9 @@
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+    print("you are a master megician")
+elif is_magician and not is_expert:
+    print("at least you're getting there")
+else:
+    print("you need a magic powers")
